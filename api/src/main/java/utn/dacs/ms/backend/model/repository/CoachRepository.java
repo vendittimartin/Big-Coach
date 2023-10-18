c69a80f2b0603387b33bec7db6f08780077597c7
Martin <vendittimartin@gmail.com>
2023-10-18 17:38:45 -0300

    delete submodule

--- api/src/main/java/utn/dacs/ms/backend/model/repository/CoachRepository.java
+++
@@ -1,9 +0,0 @@
-package utn.dacs.ms.backend.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import utn.dacs.ms.backend.model.entity.Coach;
-@Repository
-public interface CoachRepository extends JpaRepository<Coach, String>{
-
-}
--- api/src/main/java/utn/dacs/ms/backend/model/repository/EquipoRepository.java
+++
@@ -1,7 +0,0 @@
-package utn.dacs.ms.backend.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import utn.dacs.ms.backend.model.entity.Equipo;
-
-public interface EquipoRepository extends JpaRepository<Equipo,Integer> {
-}
