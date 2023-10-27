f6d4c4a4ce971bd2f519677ca9ae6a024eeed1c8
Martin <vendittimartin@gmail.com>
2023-10-27 18:26:25 -0300

    Rename conector

--- bff/src/main/java/utn/dacs/ms/bff/dto/NoticiaDTO.java
+++ bff/src/main/java/utn/dacs/ms/bff/dto/NoticiaDTO.java
@@ -2,7 +2,7 @@ package utn.dacs.ms.bff.dto;
 import lombok.Data;
 @Data
 public class NoticiaDTO {
-    private String titulo;
+    private String title;
     private String url;
-    private String fuente;
+    private String source;
 }
