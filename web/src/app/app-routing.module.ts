c5edb741b0787b4d2f79e130c46d559b8f0dd4b3
Agustin Santos <agustinsantos63@gmail.com>
2023-10-27 18:13:54 -0300

    add inicio-log

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/guard/auth.guard';
+import { InicioLogComponent } from './components/inicio-log/inicio-log.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { EquiposComponent } from './components/equipos/equipos.component';
 import { MiEquipoComponent } from './components/mi-equipo/mi-equipo.component';
@@ -9,6 +10,7 @@ import { PerfilComponent } from './components/perfil/perfil.component';
 
 const routes: Routes = [
   { path: '', component: InicioComponent},
+  {path: 'inicio-log', component: InicioLogComponent},
   { path: 'equipos', component: EquiposComponent},
   { path: 'mi-equipo', component: MiEquipoComponent},
   { path: 'ranking', component: RankingComponent},
