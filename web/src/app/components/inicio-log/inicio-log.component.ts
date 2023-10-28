810e963697ed7b3c45d1c1eb8f39e9875e27e3e9
Agustin Santos <agustinsantos63@gmail.com>
2023-10-27 21:58:42 -0300

    fixed inicio

--- web/src/app/components/inicio-log/inicio-log.component.ts
+++ web/src/app/components/inicio-log/inicio-log.component.ts
@@ -1,30 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-inicio-log',
   templateUrl: './inicio-log.component.html',
-  styleUrls: ['./inicio-log.component.css']
+  styleUrls: ['./inicio-log.component.css'] 
 })
-export class InicioLogComponent implements OnInit {
-  private currentIndexSubject = new BehaviorSubject<number>(0);
-  currentIndex$ = this.currentIndexSubject.asObservable();
-  totalSlides = 3; // Cambia esto al número total de diapositivas que tengas
-  currentSlide = 0;
+export class InicioLogComponent {
+  images: string[] = [
+    'https://img.freepik.com/foto-gratis/cancha-baloncesto-gente-fan-sport-arena-render-3d-ilustracion_654080-1445.jpg?w=1060&t=st=1698444926~exp=1698445526~hmac=09510e90bd1ca0abdbe4765af25e6b83f6feeabc9319bbfa58cb3452fef37727',
+    'https://img.freepik.com/foto-gratis/vista-silueta-jugador-baloncesto-baloncesto-espacio-negro_155003-12953.jpg?w=996&t=st=1698445740~exp=1698446340~hmac=f7c5b23bd680a75b38841f5f6a8fef01060d880d71ebcc07c5831fa949469ccd',
+    'https://img.freepik.com/foto-gratis/cancha-baloncesto-gente-fan-sport-arena-render-3d-ilustracion_654080-1445.jpg?w=1060&t=st=1698444926~exp=1698445526~hmac=09510e90bd1ca0abdbe4765af25e6b83f6feeabc9319bbfa58cb3452fef37727',
+  ];
 
-  constructor() {}
+  slides = [
+    {
+      title: 'Título de la Noticia 1',
+      news: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+      title: 'Título de la Noticia 2',
+      news: 'Otra noticia interesante que puedes agregar aquí. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    },
+    {
+      title: 'Título de la Noticia 3',
+      news: 'Otra noticia interesante que puedes agregar aquí. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    },
+  ];
 
-  ngOnInit(): void {
-    this.currentSlide = 0;
-  }
+  currentIndex: number = 0;
 
   nextSlide() {
-    this.currentSlide = (this.currentSlide + 1) % this.totalSlides;
-    this.currentIndexSubject.next(this.currentSlide);
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
   }
 
   prevSlide() {
-    this.currentSlide = this.currentSlide > 0 ? this.currentSlide - 1 : this.totalSlides - 1;
-    this.currentIndexSubject.next(this.currentSlide);
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
   }
 }
